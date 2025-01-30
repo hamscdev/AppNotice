c888ae3a7ad2fe63a6725bb9bd75fd4ee0e05d56
Hector Moreno <hector.morenos@elektra.com.mx>
2025-01-30 10:54:32 -0600

    Se cambia apikey

--- newyorktimes/network/ApiManager.swift
+++ newyorktimes/network/ApiManager.swift
@@ -12,7 +12,7 @@ final class ApiManager {
 
 	private let apiKey = "Fv4sT6Km56a1GG35S6GJROZeRXv9RTmp"
 	private let baseUrl = "https://api.nytimes.com/svc/mostpopular/v2/"
-	private let parameters = [ "api-key" : "Fv4sT6Km56a1GG35S6GJROZeRXv9RTmp"]
+	private let parameters = [ "api-key" : "qTl6HA9lEk9bHwEMNSrdjRAceMnSqQEZ"]
 
 
 	func getMostViewedBy(period: Int, completion: @escaping (Result<ResponseModel, Error>) -> Void) {
